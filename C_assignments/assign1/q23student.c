90b69c7b6a7ade0ef445da7c0b049770f1c4cc3b
Shivam5551 <139862414+Shivam5551@users.noreply.github.com>
2024-02-29 13:55:26 +0530

    Update q23student.c

--- C_assignments/assign1/q23student.c
+++ C_assignments/assign1/q23student.c
@@ -1,70 +1,27 @@
 /* Q23. Write a program to input the name,marks in three subjects 
     and calculate total marks, percentage and display */
 #include <stdio.h>
-#include <stdlib.h>
 //#include <conio.h>
 
-#define NumberOfSubjects 3
-#define Students 1
-
-char *name()
-{
-    int length = 0;
-    char ch;
-    char *name = NULL;
-
-    while ((ch = getchar()) != '\n') 
-    {
-        name = (char *)realloc(name, (length + 1) * sizeof(char));
-        if (name == NULL) 
-        {
-            printf("Memory allocation failed. Exiting.\n");
-            return NULL;
-        }
-        name[length++] = ch;
-    }
-    name[length] = '\0';
-
-    return name;
-}
+#define size 20
 
 int main()
 {
-    char *student_name[Students];
-    char *subjects_name[NumberOfSubjects];
-    float marks[Students][NumberOfSubjects];
-    float total[Students], percentage[Students];
-    int i, j;
-
+    char student_name[size];
+    float marks1, marks2, marks3;
+    float total, percentage;
     //clrscr();
-
-    for (i = 0; i < Students; i++)
-    {
-        printf("Enter name of student : ");
-        student_name[i] = name();
-        for (j = 0; j < NumberOfSubjects; j++)
-        {
-            printf("Enter %d Subject Name: ", j+1);
-            subjects_name[j] = name();
-        }
-    }
-
-    for (i = 0; i < Students; i++)
-    {
-        total[i] = 0;
-        for (j = 0; j < NumberOfSubjects; j++)
-        {
-            printf("Enter the marks of %s in %s : ", student_name[i], subjects_name[j]);
-            scanf("%f", &marks[i][j]);
-            total[i] += marks[i][j];
-        }
-        percentage[i] = total[i]/NumberOfSubjects;
-    }
-
-    for (i = 0; i < Students; i++)
-    {
-        printf("Student Name : %s\nTotal Marks : %.2f\nPercentage : %.2f\n", student_name[i], total[i], percentage[i]);
-    }
+    printf("Enter name of student : ");
+    scanf("%s", student_name);
+    printf("Enter marks of 3 Subjects: ");
+    scanf("%f", &marks1);
+    scanf("%f", &marks2);
+    scanf("%f", &marks2);
+    
+    total= marks1 + marks2 + marks3;
+    percentage = total/3;
+    
+        printf("Student Name : %s\nTotal Marks : %.2f\nPercentage : %.2f\n", student_name, total, percentage);
 
     //getch();
     return 0;
