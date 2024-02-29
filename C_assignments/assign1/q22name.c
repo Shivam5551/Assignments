d0b230a2bce90ca9515b0e11ede15aa2f0c082df
Shivam5551 <139862414+Shivam5551@users.noreply.github.com>
2024-02-29 13:57:22 +0530

    Update q22name.c

--- C_assignments/assign1/q22name.c
+++ C_assignments/assign1/q22name.c
@@ -1,33 +1,18 @@
 /* Q22. Write a program to input and print the name, age of student ( using strings ) */
 #include <stdio.h>
-#include <stdlib.h>
 //#include <conio.h>
 
 int main()
 {
     int age;
-    char *name = NULL;
-    int length = 0;
-    char ch;
+    char name[20];
     //clrscr();
 
     printf("What's your name? ");
-    while ((ch = getchar()) != '\n') 
-    {
-        name = (char *)realloc(name, (length + 1) * sizeof(char));
-        if (name == NULL) 
-        {
-            printf("Memory allocation failed. Exiting.\n");
-            return 1;
-        }
-        name[length++] = ch;
-    }
-    name[length] = '\0';
-
+    scanf("%s", name);
     printf("How old are you? ");
     scanf("%d", &age);
     printf("Hello, %s!\nYou are %d yrs old.\n", name, age);
-free(name);
 
     //getch();
     return 0;
