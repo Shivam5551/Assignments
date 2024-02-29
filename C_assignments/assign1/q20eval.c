0e3925b8bd4c8fcad792bcd47c8cfa377ad376cc
Shivam5551 <139862414+Shivam5551@users.noreply.github.com>
2024-02-29 13:39:42 +0530

    Update q20eval.c

--- C_assignments/assign1/q20eval.c
+++ C_assignments/assign1/q20eval.c
@@ -6,9 +6,10 @@
 
 int main()
 {
+    int a = 6, b = 2, c = 1, d = 3, e = 8, f = 4;
     double expression = 0;
     //clrscr();
-    expression = 6*2/ (2+1 *2/3+6) + 8* (8/4);
+    expression = a*b/ (b+c *b/d+a) + e* (e/f);
     printf("Result of experession : %lf\n", expression);
     //getch();
     return 0;
